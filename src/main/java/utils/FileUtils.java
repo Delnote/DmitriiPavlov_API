307e00c55a6bda5a6df8ce2561db118eaadff164
Natalia_Grebenshchikova <natalia_grebenshchikova@epam.com>
2019-07-29 19:11:13 +0300

    review comments

--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class FileUtils {
-
+//todo тут нет еще методов, которые бы помгали выгребать конкретную пропертю. Каждый раз при получении проперти читать файл - это неправильно.
     public static Properties readSpellerPropertiesFromFile(String filePath) {
         Properties prop = new Properties();
 
--- src/test/java/functionalTests/SpellerTest.java
+++ src/test/java/functionalTests/SpellerTest.java
@@ -1,5 +1,7 @@
 package functionalTests;
 
+import static org.testng.Assert.assertTrue;
+
 import dataProviders.DataProviders;
 import dto.SpellerDto;
 import org.testng.annotations.Test;
@@ -7,14 +9,14 @@ import service.SpellerSteps;
 
 import java.util.Arrays;
 
-import static org.testng.Assert.assertTrue;
-
 public class SpellerTest {
 
+    // todo доавь, пожалуйста, свою самописные проверки. Такую, например, как  new SpellerAssertions(response).verifyWordsList("дама", "Дима", "дома")
     @Test(dataProvider = "textTestData", dataProviderClass = DataProviders.class)
     void checkText(String testText, String textToCheck) {
 
         SpellerDto[] spellerText = new SpellerSteps().checkText(testText);
+
 
         assertTrue(spellerText[0].getS().contains(textToCheck));
     }
